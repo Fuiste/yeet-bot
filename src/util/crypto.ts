cbc261bd100b6a03180ffa6edbc44f12041df358
William Pelrine <rudy@ionic.io>
2017-12-22 17:57:49 -0600

    Clean up coin logic

--- src/util/crypto.ts
+++ src/util/crypto.ts
@@ -8,8 +8,31 @@ export interface CryptoCurrency {
   name: string;
   rank: number;
   symbol: string;
-  usd: number;
+  price: string;
   cap: string;
+}
+
+function marshallCoin(coin: any): CryptoCurrency {
+  let cap = coin.market_cap_usd
+  let cap_str = '$' + cap
+
+  if (cap >= 1000000000) {
+    cap_str = '$' + (cap / 1000000000).toFixed(2) + 'B'
+  } else if (cap >= 1000000) {
+    cap_str = '$' + (cap / 1000000).toFixed(2) + 'M'
+  } else {
+    cap_str = '$' + cap.toFixed(9)
+  }
+
+  return {
+    one_day: coin.percent_change_24h + '%',
+    seven_day: coin.percent_change_7d + '%',
+    name: coin.name,
+    rank: coin.rank,
+    symbol: coin.symbol,
+    price: '$' + coin.price_usd,
+    cap: cap_str
+  }
 }
 
 export function formatCoin(coin: CryptoCurrency, column: number, verbose?: boolean): string {
@@ -26,7 +49,7 @@ export function formatCoin(coin: CryptoCurrency, column: number, verbose?: boole
   } else {
     formatted += (coin.rank + '.   ').slice(0, 3)
     formatted += (coin.name + ' '.repeat(column)).slice(0, column - 1)
-    formatted += (' $' + coin.usd).slice(0, 9)
+    formatted += (' $' + coin.price).slice(0, 9)
   }
 
   return formatted
@@ -38,26 +61,7 @@ export async function top10(): Promise<CryptoCurrency[]> {
     let top: CryptoCurrency[] = []
 
     res.forEach(coin => {
-      let cap = coin.market_cap_usd
-      let cap_str = '$' + cap
-  
-      if (cap >= 1000000000) {
-        cap_str = '$' + (cap / 1000000000).toFixed(2) + 'B'
-      } else if (cap >= 1000000) {
-        cap_str = '$' + (cap / 1000000).toFixed(2) + 'M'
-      } else {
-        cap_str = '$' + cap
-      }
-
-      top.push({
-        one_day: coin.percent_change_24h + '%',
-        seven_day: coin.percent_change_7d + '%',
-        name: coin.name,
-        rank: coin.rank,
-        symbol: coin.symbol,
-        usd: coin.price_usd,
-        cap: cap_str
-      })
+      top.push(marshallCoin(coin))
     });
 
     return top
@@ -70,26 +74,7 @@ export async function getCoin(id: string): Promise<CryptoCurrency> {
   try {
     let coin = await api.get('/ticker/' + id.toLowerCase())
     coin = coin[0]
-    let cap = coin.market_cap_usd
-    let cap_str = '$' + cap
-
-    if (cap >= 1000000000) {
-      cap_str = '$' + (cap / 1000000000).toFixed(8) + 'B'
-    } else if (cap >= 1000000) {
-      cap_str = '$' + (cap / 1000000).toFixed(8) + 'M'
-    } else {
-      cap_str = '$' + cap.toFixed(9)
-    }
-
-    return {
-      one_day: coin.percent_change_24h + '%',
-      seven_day: coin.percent_change_7d + '%',
-      name: coin.name,
-      rank: coin.rank,
-      symbol: coin.symbol,
-      usd: coin.price_usd,
-      cap: cap_str
-    }
+    return marshallCoin(coin)
   } catch(e) {
     throw e
   }
