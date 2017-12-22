0e37fc4e449cf22d0524cf3e5583a16a1c77c267
William Pelrine <rudy@ionic.io>
2017-12-22 10:28:32 -0600

    Use a router, ya dummy

--- src/yeet.ts
+++ src/yeet.ts
@@ -5,29 +5,27 @@ import { DISCORD_TOKEN, PORT } from './environment'
 
 // Set up a webserver for monitoring
 const app = Express();
+const router = Express.Router()
 
 app.disable('x-powered-by')
-app.use(Express.json)
 
-app.all('*', (req, res) => {
-  if (req.url === '/status') {
-    // Health check
-    res.status(200)
-    res.contentType('text/plain')
-    res.send("OK")
-  }
-
-  if (req.url === '/webhook') {
-    console.log(req.body)
-    res.status(200)
-    res.contentType('text/plain')
-    res.send('OK')
-  }
+router.get('/status', (_, res) => {
+  // Health check
+  res.status(200)
+  res.contentType('text/plain')
+  res.send("OK")
 })
+
+router.post('/webhook', (req, res) => {
+  console.log(req.body)
+  res.status(200)
+  res.contentType('text/plain')
+  res.send('OK')
+})
+
+app.use('/', router)
 
 app.listen(PORT, () => {
   console.log("Yeet server is running :)")
-
-  // Log into Discord
   client.login(DISCORD_TOKEN)
 })
