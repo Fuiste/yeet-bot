909817a0e596132066613047b10f92fa71e4035b
William Pelrine <rudy@ionic.io>
2017-12-22 11:01:04 -0600

    Refactor

--- src/yeet.ts
+++ src/yeet.ts
@@ -2,14 +2,29 @@ import * as Express from 'express'
 
 import { client } from './client'
 import { DISCORD_TOKEN, PORT } from './environment'
-import { getRouter } from './router'
+import { testHook } from './util/webhook'
 
 // Set up a webserver for monitoring
 const app = Express()
+const router = Express.Router()
+
+router.get('/status', (_, res) => {
+  // Health check
+  res.status(200)
+  res.contentType('text/plain')
+  res.send("OK")
+})
+  
+router.post('/webhook', (req, res) => {
+  testHook(req.body)
+  res.status(200)
+  res.contentType('text/plain')
+  res.send('OK')
+})
 
 app.disable('x-powered-by')
 app.use(Express.json())
-app.use('/', getRouter())
+app.use('/', router)
 
 app.listen(PORT, () => {
   console.log("Yeet server is running :)")
